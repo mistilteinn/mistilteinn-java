8322afe8aeec97f4c6764fb6fea30fccb916a8b3
mallowlabs <mallowlabs@gmail.com>
2012-02-18 21:56:47 +0900

    Fixed: broken tests refs #1019

--- src/test/java/org/codefirst/mistilteinn/config/MistilteinnConfigurationFactoryTest.java
+++ src/test/java/org/codefirst/mistilteinn/config/MistilteinnConfigurationFactoryTest.java
@@ -1,17 +1,13 @@
 package org.codefirst.mistilteinn.config;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
+import org.codefirst.mistilteinn.MistilteinnException;
 import org.junit.Test;
 
 public class MistilteinnConfigurationFactoryTest {
 
-    @Test
-    public void testCreateConfiguration() throws Exception {
-        MistilteinnConfiguration configuration = MistilteinnConfigurationFactory.createConfiguration(".");
-        String result = configuration.getClass().getName();
-        assertThat(result, is(YAMLMistilteinnConfiguration.class.getName()));
+    @Test(expected = MistilteinnException.class)
+    public void testCreateConfigurationNotFound() throws Exception {
+        MistilteinnConfigurationFactory.createConfiguration("notExist");
     }
 
 }
